24ad1f04ad1f9a43888853ffa21b2437da8a4f87
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-02-26 13:07:31 +0530

    junk removed

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,18 +4,6 @@ import dotenv from "dotenv";
 import { JWT_USER_SECRET } from '../config';
 dotenv.config();
 
-
-// Password hashing
-// export const hashPassword = async (password: string): Promise<string> => {
-//     const salt = await bcrypt.genSalt(10);
-//     return await bcrypt.hash(password, salt);
-// };
-
-// Password comparison
-// export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
-//     return await bcrypt.compare(password, hashedPassword);
-// };
-
 // Generate JWT token
 export const generateToken = (userId: string): string => {
     return jwt.sign({ id: userId }, JWT_USER_SECRET || 'your-secret-key', { expiresIn: '1h' });
